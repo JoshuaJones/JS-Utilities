5212de409b341343a93cc0ecfa483718cb5108a8
Joshua Jones <Jones.R.Joshua@gmail.com>
2016-11-29 16:51:31 -0800

    [Feature] Add simple getCookie function

---
+++ src/getCookie.js
@@ -0,0 +1,5 @@
+function getCookie(name) {
+    const matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+
+    return matches ? decodeURIComponent(matches[1]) : undefined
+}
